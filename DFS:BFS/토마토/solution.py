6449ceabad80f3686ce2c74371fec553f258f6a8
dzzy6505 <dzzy6505@gmail.com>
2021-01-07 22:22:48 +0900

    tomato solution

---
+++ DFS:BFS/토마토/solution.py
@@ -0,0 +1,52 @@
+#%%
+n, m = 2, 2
+graph = [
+[1, -1],
+[-1, 1],
+]
+visited = [[False] * n for _ in range(m)]
+start = []
+for row in range(m):
+    for col in range(n):
+        if graph[row][col] == 1:
+            start.append([col, row])
+# %%
+from collections import deque
+
+def is_ripen(graph):
+    for row in graph:
+        if 0 in row:
+            return False
+    return True
+
+
+def bfs(graph, start, visited):
+    date = 0
+    queue = deque()
+    for x,y in start:
+        visited[y][x] = True
+        queue.append([x,y, date])
+
+    # 왼쪽, 오른쪽, 앞, 뒤 방향 정의
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, +1]
+
+    while queue:
+        x, y, date = queue.popleft()
+        for a, b in zip(dx, dy):
+            nx, ny = x + a, y + b
+            if 0 <= nx < n and 0 <= ny < m and 0 <= graph[ny][nx]:
+                if not visited[ny][nx]:
+                    visited[ny][nx] = True
+                    graph[ny][nx] = 1
+                    queue.append([nx, ny, date+1])
+        if is_ripen(graph):
+            return date
+
+
+result = bfs(graph, start, visited)
+if not result:
+    print(0)
+else:
+    print(result+1)
+# %%
