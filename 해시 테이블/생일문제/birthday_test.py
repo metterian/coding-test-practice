628265a6f0ea523b6291848f6db5a829feef31dc
SeungJun <dzzy6505@gmail.com>
2021-04-16 10:00:08 +0900

    add: update

--- 해시 테이블/생일문제/birthday_test.py
+++ 해시 테이블/생일문제/birthday_test.py
@@ -8,3 +8,16 @@ import random
 TRIALS = 10000 # 10만 번의 실험
 same_birthdays = 0 # 생일이 같은 실험의 수
 
+# 10만번의 실험 진행
+for _ in range(TRIALS):
+    birthdays = []
+    # 23명이 모몄을때, 생일이 같은 경우 same_birthdays += 1
+    for i in range(23):
+        birthday = random.randint(1,365)
+        if birthday in birthdays:
+            same_birthdays += 1
+            break
+        birthdays.append(birthday)
+
+print(f'{same_birthdays/TRIALS *100}%')
+# %%
---
+++ 해시 테이블/해시맵 디자인/hashmap.py
@@ -0,0 +1,16 @@
+#%%
+class MyHashMap:
+    def __init__(self, key = None, value= None) -> None:
+        self.key = key
+        self.value = value
+        self.next = None
+    def put(self, key: int, value: int) -> None:
+        pass
+
+    def get(self, key: int) -> int:
+        pass
+
+    def remove(self, key: int) -> None:
+        pass
+
+# %%
